a87c286cdfeed3befb4e513bb90e41cd90bd2c22
Riki Parata <you@example.com>
2018-07-03 20:23:20 +1200

    home buttons

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,9 +5,9 @@ import {list_of_moods as moods} from '../../data/moods.json'
 const Home = (props) => {
 
   return (
-    <div id="cool" className="tile has-text-centered">
+    <div id="cool" className="tile is-vertical-center">
       {moods.map(moodData => {
-        return <div className="tile is-child box" key={moodData.mood}><div className="yes"><Link to={`/${moodData.mood}/content`}>{moodData.mood}</Link></div></div>
+        return <div className="tile is-child box" key={moodData.mood}><div className="yes"><Link className="home button is-large is-rounded" to={`/${moodData.mood}/content`}>{moodData.mood}</Link></div></div>
       })} 
     </div>
   )
--- client/components/MoodContent.jsx
+++ client/components/MoodContent.jsx
@@ -1,60 +1,63 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import {list_of_moods as moods} from '../../data/moods.json'
+import { list_of_moods as moods } from '../../data/moods.json'
 
 const MoodContent = (props) => {
-  const moodName = props.match.params.mood
-  const mood = moods.find(a => a.mood === moodName)
+    const moodName = props.match.params.mood
+    const mood = moods.find(a => a.mood === moodName)
 
-  return (
-    <div>
-      <h2>{mood.mood}</h2>
-      <div className="tile is-ancestor">
-            <div className="tile is-parent">
-                <article className="tile is-child box">
-                    <ul>
-                        <h3>Music</h3>
-                        {mood.music.map(music => {
-                            return <li key={music}><a className="button is-large is-rounded" href={music.link} target="_blank">{music.song}</a></li>
-                        })}
-                    </ul>
-                </article>
+    return (
+        <div>
+            <h2>{mood.mood}</h2>
+            <div className="tile is-ancestor">
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <ul>
+                            <h3>Music</h3>
+                            {mood.music.map(music => {
+                                return <li key={music}><a className="button is-large is-rounded" href={music.link} target="_blank">{music.song}</a></li>
+                            })}
+                        </ul>
+                    </article>
+                </div>
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <ul>
+                            <h3>Movies</h3>
+                            {mood.content.map(content => {
+                                return <li key={content}><a className="button is-large is-rounded" href={content.link} target="_blank">{content.movie}</a></li>
+                            })}
+                        </ul>
+                    </article>
+                </div>
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <ul>
+                            <h3>Articles</h3>
+                            {mood.article.map(article => {
+                                return <li key={article}><a className="button is-large is-rounded" href={article.link} target="_blank">{article.article}</a></li>
+                            })}
+                        </ul>
+                    </article>
+                </div>
+                <div className="tile is-parent">
+                    <article className="tile is-child box">
+                        <ul>
+                            <h3>Games</h3>
+                            {mood.game.map(game => {
+                                return <li key={game}><a className="button is-large is-rounded" href={game.link} target="_blank">{game.game}</a></li>
+                            })}
+                        </ul>
+                    </article>
+                </div>
             </div>
-            <div className="tile is-parent">
-                <article className="tile is-child box">
-                    <ul>
-                        <h3>Movies</h3>
-                        {mood.content.map(content => {
-                            return <li key={content}><a className="button is-large is-rounded" href={content.link} target="_blank">{content.movie}</a></li>
-                        })}
-                    </ul>
-                </article>
-            </div>
-            <div className="tile is-parent">
-                <article className="tile is-child box">
-                    <ul>
-                        <h3>Articles</h3>
-                        {mood.article.map(article => {
-                            return <li key={article}><a className="button is-large is-rounded" href={article.link} target="_blank">{article.article}</a></li>
-                        })}
-                    </ul>
-                </article>
-            </div>
-            <div className="tile is-parent">
-                <article className="tile is-child box">
-                    <ul>
-                        <h3>Games</h3>
-                        {mood.game.map(game => {
-                            return <li key={game}><a className="button is-large is-rounded" href={game.link} target="_blank">{game.game}</a></li>
-                        })}
-                    </ul>
-                </article>
+            <div className="fullWidth">
+                <div id="back" className="button is-large is-rounded"><Link to="/">BACK</Link></div>
+
             </div>
         </div>
-      <div id="back" className="button is-large"><Link to="/">Back</Link></div>
-    </div>
-  )
+    )
 }
 
 export default MoodContent
